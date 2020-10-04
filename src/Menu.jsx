7107736664344dd8ed522e919f2f41e02fab5a60
Leonel Gauna <leonel.mariano.g@gmail.com>
2020-10-04 17:08:57 -0300

    Update Menu component

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -11,10 +11,25 @@ const MenuBackground = styled.div`
   align-items: center;
 `;
 
+const PlayerSelectButton = styled.button`
+  width: 20%;
+  background: none;
+  border: none;
+  cursor: pointer;
+  pointer-events: ${(props) => (props.isDisabled ? "none" : "auto")};
+  transition: filter 0.5s, transform 0.5s;
+  animation: ${(props) => (props.isAnimated ? "bounce 0.3s linear 0s 1" : "")};
+
+  &:hover {
+    filter: brightness(1.5);
+    transform: scale(1.2);
+  }
+`;
+
 export default class Menu extends React.Component {
   constructor() {
     super();
-    this.state = { bounceX: 0, bounceO: 0 };
+    this.state = { bounceX: 0, bounceO: 0, buttonsDisabled: 0 };
     this.handleClick = this.handleClick.bind(this);
   }
 
@@ -26,43 +41,45 @@ export default class Menu extends React.Component {
   render() {
     return (
       <MenuBackground>
-        <svg
+        <PlayerSelectButton
           id="x"
-          onClick={() => this.setState({ bounceX: 1 })}
-          className={`button-x ${this.state.bounceX ? "click-disabled" : ""} ${
-            this.state.bounceX ? "bounce" : ""
-          }`}
-          bounce={this.state.bounceX}
+          onClick={() => this.setState({ bounceX: 1, buttonsDisabled: 1 })}
           onAnimationEnd={this.handleClick}
-          xmlns="http://www.w3.org/2000/svg"
-          xmlnsXlink="http://www.w3.org/1999/xlink"
-          x="0px"
-          y="0px"
-          viewBox="0 0 51.6 51.6"
-          style={{ enableBackground: "new 0 0 51.6 51.6" }}
-          xmlSpace="preserve"
+          isDisabled={this.state.buttonsDisabled}
+          isAnimated={this.state.bounceX}
         >
-          <line className="line" x1="10.4" y1="10" x2="41.2" y2="41.6" />
-          <line className="line" x1="41.6" y1="10.4" x2="10" y2="41.2" />
-        </svg>
-        <svg
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            xmlnsXlink="http://www.w3.org/1999/xlink"
+            x="0px"
+            y="0px"
+            viewBox="0 0 51.6 51.6"
+            style={{ enableBackground: "new 0 0 51.6 51.6" }}
+            xmlSpace="preserve"
+          >
+            <line className="line" x1="10.4" y1="10" x2="41.2" y2="41.6" />
+            <line className="line" x1="41.6" y1="10.4" x2="10" y2="41.2" />
+          </svg>
+        </PlayerSelectButton>
+        <PlayerSelectButton
           id="o"
-          onClick={() => this.setState({ bounceO: 1 })}
-          className={`button-o ${this.state.bounceO ? "click-disabled" : ""} ${
-            this.state.bounceO ? "bounce" : ""
-          }`}
-          bounce={this.state.bounceO}
+          onClick={() => this.setState({ bounceO: 1, buttonsDisabled: 1 })}
           onAnimationEnd={this.handleClick}
-          xmlns="http://www.w3.org/2000/svg"
-          xmlnsXlink="http://www.w3.org/1999/xlink"
-          x="0px"
-          y="0px"
-          viewBox="0 0 60 60"
-          style={{ enableBackground: "new 0 0 60 60" }}
-          xmlSpace="preserve"
+          isDisabled={this.state.buttonsDisabled}
+          isAnimated={this.state.bounceO}
         >
-          <circle className="circle" cx="30" cy="30" r="20" />
-        </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            xmlnsXlink="http://www.w3.org/1999/xlink"
+            x="0px"
+            y="0px"
+            viewBox="0 0 60 60"
+            style={{ enableBackground: "new 0 0 60 60" }}
+            xmlSpace="preserve"
+          >
+            <circle className="circle" cx="30" cy="30" r="20" />
+          </svg>
+        </PlayerSelectButton>
       </MenuBackground>
     );
   }
