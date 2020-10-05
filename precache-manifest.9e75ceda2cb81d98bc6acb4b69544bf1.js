0e043da0fd242b17ddbdefe7e24d435c77a9d823
Leonel Gauna <leonel.mariano.g@gmail.com>
2020-10-05 19:18:19 -0300

    Updates

---
+++ precache-manifest.9e75ceda2cb81d98bc6acb4b69544bf1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "862877b074c5922bb86c00495d417814",
+    "url": "/index.html"
+  },
+  {
+    "revision": "193fcfa9df4e210b267c",
+    "url": "/static/js/2.45233650.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.45233650.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "549b7b27afad7dc5c6e6",
+    "url": "/static/js/main.9fc9ac37.chunk.js"
+  },
+  {
+    "revision": "828c0bdfa56a628b17a7",
+    "url": "/static/js/runtime-main.eb9be228.js"
+  }
+]);
