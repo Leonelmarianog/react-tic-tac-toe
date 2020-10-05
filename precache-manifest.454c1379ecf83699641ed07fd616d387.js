a772d6ba67a1c886e9252aa73d842d4cfabe51eb
Leonel Gauna <leonel.mariano.g@gmail.com>
2020-10-05 19:27:09 -0300

    Updates

---
+++ precache-manifest.454c1379ecf83699641ed07fd616d387.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a0e4a9212b6f2566b120c745385c2c0",
+    "url": "/react-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "e7504fed9665a5ca2e1a",
+    "url": "/react-tic-tac-toe/static/js/2.9969a374.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-tic-tac-toe/static/js/2.9969a374.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "549b7b27afad7dc5c6e6",
+    "url": "/react-tic-tac-toe/static/js/main.9fc9ac37.chunk.js"
+  },
+  {
+    "revision": "d9255e6de55285e18dc3",
+    "url": "/react-tic-tac-toe/static/js/runtime-main.81654cbd.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9e75ceda2cb81d98bc6acb4b69544bf1.js"
+  "/react-tic-tac-toe/precache-manifest.454c1379ecf83699641ed07fd616d387.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-tic-tac-toe/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
